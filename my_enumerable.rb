1694d29fc5b97f2529d19b90bdf1b373ec57d091
Ismail mayito <ismayito1@gmail.com>
2023-11-04 12:36:02 +0300

    create custom Enumerable

---
+++ my_enumerable.rb
@@ -0,0 +1,20 @@
+module MyEnumerable
+  def all?
+    arr = []
+    @list.each { |i| arr.push(i) if yield i }
+    puts arr.length == @list.length
+  end
+
+  def any?
+    @list.each do |i|
+      puts true if yield i
+    end
+    puts false
+
+    def filter
+      @array = []
+      @list.each { |i| @array.push(i) if yield i }
+      print "#{@array}\n"
+    end
+  end
+end
